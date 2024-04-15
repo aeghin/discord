247c46d051c15c0e0f84e3f68113f649ab9693fe
Edward Castaneda <aeghin7@gmail.com>
2024-04-15 18:42:34 -0500

    feat: uploading server images is now available

--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -25,6 +25,7 @@ import { useForm } from "react-hook-form";
 import * as z from 'zod';
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useEffect, useState } from "react";
+import { FileUpload } from "@/components/ui/file-upload";
 
 const formSchema = z.object({
     name: z.string().min(1, {
@@ -75,7 +76,21 @@ export const InitialModal = () => {
                     <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
                         <div className="space-y-8 px-6">
                             <div className="flex items-center justify-center text-center">
-                                TODO: Upload image
+                                <FormField
+                                    control={form.control}
+                                    name="imageUrl"
+                                    render={({ field }) => (
+                                        <FormItem>
+                                            <FormControl>
+                                                <FileUpload 
+                                                endpoint="serverImage"
+                                                value={field.value}
+                                                onChange={field.onChange}
+                                                />
+                                            </FormControl>
+                                        </FormItem>
+                                    )}
+                                />
                             </div>
                             <FormField
                                 control={form.control}
