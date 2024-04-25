d4f6e144b33bdf06b063bad5d410f38a430523a1
Edward Castaneda <aeghin7@gmail.com>
2024-04-25 16:05:30 -0500

    feat: side bar displays channels and properly redirects

--- components/action-tooltip.tsx
+++ components/action-tooltip.tsx
@@ -28,7 +28,7 @@ export const ActionTooltip = ({
                 </TooltipTrigger>
                 <TooltipContent side={side} align={align}>
                     <p className='font-semibold text-sm capitalize'>
-                        {label.toLocaleLowerCase()}
+                        {label.toLowerCase()}
                     </p>
                 </TooltipContent>
             </Tooltip>
