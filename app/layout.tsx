33f48cce174fde2cee01b70828ea51f2d6801f38
Edward Castaneda <aeghin7@gmail.com>
2024-04-30 13:25:10 -0500

    added modal provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { cn } from "@/lib/utils";
 
 import { ClerkProvider } from "@clerk/nextjs";
 import { ThemeProvider } from "@/components/providers/theme-provider";
+import { ModalProvider } from "@/components/providers/modal-provider";
 
 const font = Open_Sans({ subsets: ["latin"] });
 
@@ -24,6 +25,7 @@ export default function RootLayout({
       <html lang="en" suppressHydrationWarning>
         <body className={cn(font.className, "bg-white dark:bg-[#313338]")}>
           <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false} storageKey="discord-theme">
+            <ModalProvider />
             {children}
           </ThemeProvider>
         </body>
