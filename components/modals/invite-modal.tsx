ac23beea5b7464c67fbdad3685fd402886736a8f
Edward Castaneda <aeghin7@gmail.com>
2024-06-11 12:17:12 -0500

    feat: server invite link functional

--- components/modals/invite-modal.tsx
+++ components/modals/invite-modal.tsx
@@ -11,14 +11,35 @@ import { useModal } from "@/hooks/use-modal-store";
 import { Label } from "../ui/label";
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
-import { Copy, RefreshCw } from "lucide-react";
+import { Check, Copy, RefreshCw } from "lucide-react";
+import { useOrigin } from "@/hooks/use-origin";
+import { useState } from "react";
 
 
 export const InviteModal = () => {
 
-    const { isOpen, onClose, type } = useModal();
+    const { isOpen, onClose, type, data } = useModal();
+
+    const origin = useOrigin();
 
     const isModalOpen = isOpen && type === 'invite';
+
+    const { server } = data;
+
+    const [copied, setCopied] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
+
+
+    const onCopy = () => {
+        navigator.clipboard.writeText(inviteUrl);
+        setCopied(true);
+
+        setTimeout(() => {
+            setCopied(false);
+        }, 1000);
+    };
+
+    const inviteUrl = `${origin}/invite/${server?.inviteCode}`;
 
     return (
         <Dialog open={isModalOpen} onOpenChange={onClose}>
@@ -31,9 +52,9 @@ export const InviteModal = () => {
                 <div className="p-6">
                     <Label className="uppercase text-xs font-bold text-zinc-500 dark:text-secondary/70">Server Invite Link</Label>
                     <div className="flex items-center mt-2 gap-x-2">
-                        <Input className="bg-zinc-300/50 border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0" value='invite-link' />
-                        <Button size='icon'>
-                            <Copy className="w-4 h-4" />
+                        <Input className="bg-zinc-300/50 border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0" value={inviteUrl} />
+                        <Button onClick={onCopy} size='icon'>
+                            {copied ? <Check className="w-4 h-4" /> : <Copy className="w-4 h-4" />}
                         </Button>
                     </div>
                     <Button variant='link' size='sm' className="text-xs text-zinc-500 mt-4">
