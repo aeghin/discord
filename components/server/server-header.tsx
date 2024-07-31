36cc1cf195a7d8df9441bec1ce931ce5e62f47b9
Edward Castaneda <aeghin7@gmail.com>
2024-07-31 13:17:12 -0500

    added channel api and modal

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -43,7 +43,7 @@ export const ServerHeader = ({
                     </DropdownMenuItem>
                 )}
                 {isAdmin && (
-                    <DropdownMenuItem  onClick={() => onOpen('editServer', { server })} className="px-3 py-2 text-sm cursor-pointer">
+                    <DropdownMenuItem onClick={() => onOpen('editServer', { server })} className="px-3 py-2 text-sm cursor-pointer">
                         Server Settings
                         <Settings className="h-4 w-4 ml-auto" />
                     </DropdownMenuItem>
@@ -55,7 +55,7 @@ export const ServerHeader = ({
                     </DropdownMenuItem>
                 )}
                 {isModerator && (
-                    <DropdownMenuItem className="px-3 py-2 text-sm cursor-pointer">
+                    <DropdownMenuItem onClick={() => onOpen('createChannel')} className="px-3 py-2 text-sm cursor-pointer">
                         Create Channel
                         <PlusCircle className="h-4 w-4 ml-auto" />
                     </DropdownMenuItem>
